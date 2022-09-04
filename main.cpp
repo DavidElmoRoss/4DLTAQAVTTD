8c766a51fcb1b09e5393ab9e7fe322d41c0e0322
David Elmo Ross <david.elmo.ross@icloud.com>
2022-09-04 18:02:31 -0400

    Labtest1 Question
    Labtest1 Question

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+Uncomment ONLY 1 printf at a time for OPTION 1, OPTION 2, 
+ OPTION 3 and OPTION 4 (Do NOT ALTER any other lines) to determine
+which one will print the message HELLO WORLD
+centred on the Middle line of the screen in RED letters
+Then select which OPTION 1-4  that did this for this question on LAB Test1
+*/
+
+#include "mbed.h"
+int main(void)
+{
+    printf("\e[1;1H\e[0m\e[2J");            // Do NOT change this LINE
+    fflush(stdout);                         // Do NOT change this line
+//  printf("\e[12;35H\e[41mHello World\n"); // Option 1
+//  printf("\e[12;35H\e[44mHello World\n"); // Option 2
+//  printf("\e[12;35H\e[42mHello World\n"); // Option 3
+//  printf("\e[12;35H\e[45mHello World\n"); // Option 4 
+    for(;;);                                // DO NOT change this line
+}
